24b06eec5590732874fd5edc0ab763890eaafe2d
caiogmello <caiogomedemello@gmail.com>
2023-03-15 22:02:03 -0300

    implementing exception handler

---
+++ delivery-systemAPI/src/main/java/com/caiogmello/deliveryAPI/api/exceptionhandler/ApiExceptionHandler.java
@@ -0,0 +1,14 @@
+package com.caiogmello.deliveryAPI.api.exceptionhandler;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
+    
+}
