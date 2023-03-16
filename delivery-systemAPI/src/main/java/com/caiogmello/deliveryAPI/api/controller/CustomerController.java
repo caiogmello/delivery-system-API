b0192e33d2e5fa616005ba70c177ffa81447b1d7
caiogmello <caiogomedemello@gmail.com>
2023-03-15 21:27:57 -0300

    implementing customer CRUD

--- delivery-systemAPI/src/main/java/com/caiogmello/deliveryAPI/api/controller/CustomerController.java
+++ delivery-systemAPI/src/main/java/com/caiogmello/deliveryAPI/api/controller/CustomerController.java
@@ -6,9 +6,10 @@ import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Arrays;
 import java.util.List;
@@ -16,20 +17,53 @@ import java.util.Optional;
 
 @AllArgsConstructor
 @RestController
+@RequestMapping("/customers")
 public class CustomerController {
 
     @Autowired
     private CustomerRepository customerRepository;
 
-    @GetMapping("/customers")
+    @GetMapping
     public List<Customer> list(){
         return customerRepository.findAll();
     }
 
-    @GetMapping("/customers/{customerId}")
-    public Customer get(@PathVariable Long customerId){
+    @GetMapping("/{customerId}")
+    public ResponseEntity<Customer> get(@PathVariable Long customerId){
         Optional<Customer> customer = customerRepository.findById(customerId);
 
-        return customer.orElse(null);
+        return customer.map(ResponseEntity::ok).
+                orElseGet(() -> ResponseEntity.notFound().build());
+
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Customer add(@RequestBody Customer customer) {
+        return customerRepository.save(customer);
+    }
+
+    @PutMapping("/{customerId}")
+    public ResponseEntity<Customer> refresh(@PathVariable Long customerId,
+                                           @RequestBody Customer customer) {
+        if (!customerRepository.existsById(customerId)) {
+            return ResponseEntity.notFound().build();
+        }
+
+        customer.setId(customerId);
+        customer = customerRepository.save(customer);
+
+        return ResponseEntity.ok(customer);
+    }
+
+    @DeleteMapping("/{customerId}")
+    public ResponseEntity<Void> remove(@PathVariable Long customerId) {
+        if (!customerRepository.existsById(customerId)) {
+            return ResponseEntity.notFound().build();
+        }
+
+        customerRepository.deleteById(customerId);
+
+        return ResponseEntity.noContent().build();
     }
 }
